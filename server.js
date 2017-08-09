97472a9ee7ed62cf3b158ab281af98ab3c6a4df8
Ian Harvey <ith.harvey@gmail.com>
2017-08-09 17:51:51 -0500

    working through linking error

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
-
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/views'))
+app.use(express.static('views'));
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index.ejs');
 });
 
 app.get('/direction', function(req, res) {
-    res.render('pages/direction');
+    res.render('pages/direction.ejs');
 });
 
 app.listen(8080);
