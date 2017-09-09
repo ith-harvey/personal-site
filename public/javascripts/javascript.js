9bddc344620a9ec9c50c239597bfef3d74c81126
Ian Harvey <ith.harvey@gmail.com>
2017-09-09 16:03:42 -0500

    adding phone design

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,6 +1,7 @@
 
 
 $(document).ready( function () {
+
   $('.splash-content-1').css('visibility','visible').addClass('animated bounceinUp');
   setTimeout(function () {
     $('.splash-content-2').css('visibility','visible').addClass('animated bounceinUp');
@@ -18,12 +19,12 @@ $(document).ready( function () {
     $('.splash-statement-3').css('visibility','visible').addClass('animated fadeInUp');
   }, 1200)
 
+  setTimeout(function () {
+    $('.arrow').css('visibility','visible').addClass('animated bounceinUp');
+  }, 3500)
 
-
-
-
-
-
-
+  setTimeout(function () {
+  $('.arrow').removeClass('bounceinUp').addClass('bounce')
+}, 5000)
 
 })
