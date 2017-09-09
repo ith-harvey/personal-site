83733fdceca354f98d0c879fe70396b2f9b0008f
Ian Harvey <ith.harvey@gmail.com>
2017-09-08 21:23:35 -0500

    finnished landing page

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,11 +1,26 @@
 
 
 $(document).ready( function () {
+  $('.splash-content-1').css('visibility','visible').addClass('animated bounceinUp');
+  setTimeout(function () {
+    $('.splash-content-2').css('visibility','visible').addClass('animated bounceinUp');
+  }, 600)
 
-  $('.splash-content-2').addClass('animated bounceInUp');
+  setTimeout(function () {
+    $('.splash-statement-1').css('visibility','visible').addClass('animated fadeInUp');
+  }, 800)
+
+  setTimeout(function () {
+    $('.splash-statement-2').css('visibility','visible').addClass('animated fadeInUp');
+  }, 1000)
+
+  setTimeout(function () {
+    $('.splash-statement-3').css('visibility','visible').addClass('animated fadeInUp');
+  }, 1200)
 
 
-  $('.splash-content-1').css({'display':'block', 'opacity':'0'}).animate({'opacity':'1','bottom':'5%'}, 1500);
+
+
 
 
 
