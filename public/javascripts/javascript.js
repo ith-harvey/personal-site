c0e54d51c7058ff814f80125e3a8a95b5878633b
Ian Harvey <ith.harvey@gmail.com>
2017-09-08 19:00:47 -0500

    setup animations

---
+++ public/javascripts/javascript.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready( function () {
+
+  $('.splash-content-2').addClass('animated bounceInUp');
+
+
+  $('.splash-content-1').css({'display':'block', 'opacity':'0'}).animate({'opacity':'1','bottom':'5%'}, 1500);
+
+
+
+
+
+})
