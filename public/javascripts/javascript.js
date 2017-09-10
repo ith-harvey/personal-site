548c30eaa738c1694b268264e2456681f34b898c
Ian Harvey <ith.harvey@gmail.com>
2017-09-10 18:05:16 -0500

    done with skill graph

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -42,6 +42,8 @@ $(document).ready( function () {
     if ($(document).scrollTop() > 1480) {
       drawMarkerLines()
       drawTechScores()
+      drawLogos()
+      drawSideTech()
     }
 
   }
@@ -54,31 +56,60 @@ $(document).ready( function () {
   }
 
   function drawMarkerLines() {
-    $('.skillmarker-1').addClass('showing').css("height", "60%").css("left", "30%")
+    $('.skillmarker-1').addClass('showing').css("height", "50%").css("left", "30%")
     $('.skillnumber-1').css('visibility','visible').css("left", "29.75%").addClass('animated fadeInLeft viewed')
 
-    $('.skillmarker-2').addClass('showing').css("height", "60%").css("left", "43%")
+    $('.skillmarker-2').addClass('showing').css("height", "50%").css("left", "43%")
     $('.skillnumber-2').css('visibility','visible').css("left", "42.75%").addClass('animated fadeInLeft viewed')
 
-    $('.skillmarker-3').addClass('showing').css("height", "60%").css("left", "56%")
+    $('.skillmarker-3').addClass('showing').css("height", "50%").css("left", "56%")
     $('.skillnumber-3').css('visibility','visible').css("left", "55.75%").addClass('animated fadeInLeft viewed')
 
-    $('.skillmarker-4').addClass('showing').css("height", "60%").css("left", "69%")
+    $('.skillmarker-4').addClass('showing').css("height", "50%").css("left", "69%")
     $('.skillnumber-4').css('visibility','visible').css("left", "68.75%").addClass('animated fadeInLeft viewed')
-
     }
 
   function drawTechScores () {
-    // setTimeout( function () {
       $('.skillbar-1').addClass('showing-skill').css("width", "89%")
       $('.skillbar-2').addClass('showing-skill').css("width", "83%")
       $('.skillbar-3').addClass('showing-skill').css("width", "66%")
       $('.skillbar-4').addClass('showing-skill').css("width", "70%")
-      setTimeout( () => {
-        $('.logo-skill').addClass('animated fadeInLeft').css("visibility", "visible")
-      }, 1500)
+  }
 
-    // }, 2000)
+  function drawLogos() {
+    setTimeout( () => {
+      $('.logo-skill').addClass('animated fadeInLeft').css("visibility", "visible")
+    }, 1500)
+  }
+
+  function drawSideTech() {
+    setTimeout( () => {
+      $('.side-tech-card-1').addClass('animated flipInX').css("visibility", "visible")
+    }, 2000)
+    setTimeout( () => {
+      $('.side-tech-card-2').addClass('animated flipInX').css("visibility", "visible")
+    }, 2100)
+    setTimeout( () => {
+      $('.side-tech-card-3').addClass('animated flipInX').css("visibility", "visible")
+    }, 2200)
+    setTimeout( () => {
+      $('.side-tech-card-4').addClass('animated flipInX').css("visibility", "visible")
+    }, 2300)
+    setTimeout( () => {
+      $('.side-tech-card-5').addClass('animated flipInX').css("visibility", "visible")
+    }, 2400)
+    setTimeout( () => {
+      $('.side-tech-card-6').addClass('animated flipInX').css("visibility", "visible")
+    }, 2500)
+    setTimeout( () => {
+      $('.side-tech-card-7').addClass('animated flipInX').css("visibility", "visible")
+    }, 2600)
+    setTimeout( () => {
+      $('.side-tech-card-8').addClass('animated flipInX').css("visibility", "visible")
+    }, 2700)
+    setTimeout( () => {
+      $('.side-tech-card-9').addClass('animated flipInX').css("visibility", "visible")
+    }, 2800)
   }
 
 })
